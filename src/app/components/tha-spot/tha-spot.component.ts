48b3762abadbb80dc9a616897aa45e8e4770ef99
Smuve-Jeff <241426168+Smuve-Jeff@users.noreply.github.com>
2026-06-28 21:16:01 -0500

    Update src/app/components/tha-spot/tha-spot.component.ts

--- src/app/components/tha-spot/tha-spot.component.ts
+++ src/app/components/tha-spot/tha-spot.component.ts
@@ -578,8 +578,6 @@ export class ThaSpotComponent implements OnInit, OnDestroy, AfterViewInit {
     const queryString = params.toString();
     const url = queryString ? `${baseUrl}?${queryString}` : baseUrl;
 
-    navigator.clipboard.writeText(url).then(() => {
-      this.snackbarService.success('MISSION LINK COPIED TO CLIPBOARD');
     });
   }
 
